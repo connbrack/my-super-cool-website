47643f0b3facde7e37b8091db1cf03f0302e6ef1
Connor Brackley <noreply@git.com>
2024-10-30 10:46:18 -0400

    Moved a linky

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,6 @@
 import Text from "../components/Text";
 import "../css/main.css";
 
-
 export default function Home() {
   return (
     <div>
@@ -9,16 +8,17 @@ export default function Home() {
       <div className="text">{Text.aboutMe}</div>
       <br />
       <div>
-        <a href="/portfolio">Check out my portfolio !!</a>
-      </div>
-      <div>
         <a href="/connor-brackley-cv.pdf" target="_blank" rel="noreferrer">
           Download my resume !!
         </a>
       </div>
 
       <h2>My skills !!</h2>
-        <div className="text">{Text.mySkills}</div>
+      <div className="text">{Text.mySkills}</div>
+      <br />
+      <div>
+        <a href="/portfolio">Check out my portfolio !!</a>
+      </div>
       <h2>My academic work !!</h2>
       <div>
         <div className="text">{Text.academicWork}</div>
